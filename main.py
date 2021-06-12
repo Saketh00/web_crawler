b976537281c05ee6c2ac881c6a0f491205272b6e
Saketh00 <sakethch310@gmail.com>
2021-06-12 13:15:13 +0530

    Modefies main: Rendering using templates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import psycopg2
 from flask import Flask, request
+from flask import render_template
 
 app=Flask("Job site")
 
@@ -9,45 +10,18 @@ dbconnect=psycopg2.connect("dbname=naukri")
 def initial():
     cursor=dbconnect.cursor()
     cursor.execute("select count(*) from openings")
-    njobs=cursor.fetchall()[0][0]
+    count_of_jobs=cursor.fetchall()[0][0]
 
-    return f"""
-    <html>
-    <head>
-    <title> Jobs Page</title>
-    </head>
-
-    <body>
-    <h1>Welcome to jobs page</h1>
-    There are currently <a href="/jobs">{njobs}</a> available.
-    </body>
-    </html>
-    """
+    return render_template("main.html",njobs=count_of_jobs)
 
 
 @app.route("/jobs") #decorator
 def jobs():
     cursor=dbconnect.cursor()
     cursor.execute("select title,company_name,jd_text from openings")
-    ret=[]
-    for title, company_name, jd in cursor.fetchall():
-        item=f"<li><b>{title}</b> :::: {company_name} <br/> {jd}</li>"
-        ret.append(item)
-    jobs="<hr/>".join(ret)
-    return f"""
-    <html>
-    <head>
-    <title> Jobs Page</title>
-    </head>
-
-    <body>
-    <h1>Welcome to jobs page</h1>
-    <ol>
-    {jobs}
-    </ol>
-    </body>
-    </html>
-    """
+    jobs_list=cursor.fetchall()
+    
+    return render_template("jobslist.html",jobs=jobs_list)
 
 if __name__=="__main__":
     app.run()
