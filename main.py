63f1e94b4dd495143b698704145c2777a151eb87
Saketh00 <sakethch310@gmail.com>
2021-07-03 11:15:56 +0530

    Jsonify

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import psycopg2
 from flask import Flask, request
-from flask import render_template
+from flask import render_template,jsonify
+
+name="default"
 
 app=Flask("Job site")
 
 dbconnect=psycopg2.connect("dbname=naukri")
+
+@app.route("/api")
+def api():
+    print(request.headers)
+    if request.headers.get("appId")!="109":
+        return jsonify({"Error" : "Need appId to work"})
+    else:
+        x={"name": name, "language": "python"}
+        return jsonify(x)
 
 @app.route("/")
 def initial():
